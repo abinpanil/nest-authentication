368a1bda61b77b6cd10e3abbe8322bd4733469b4
abinpanil <abinpanil9@gmail.com>
2022-05-06 12:06:45 +0530

    initial commit after create nest app

---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  logger: Logger;
+
+  constructor() {
+    this.logger = new Logger();
+  }
+
+  getHello(): string {
+    this.logger.debug('test');
+    return 'Hello World!';
+  }
+}
